c79e3047e1afb6fd6e8c76f5abd87cebf1a0467a
matikoxfirefox <ffox77343@gmail.com>
2026-04-30 18:51:23 +0200

    + Favicon Temporary styles Pulling input values and putting them into arr

--- TaskFlow.js
+++ TaskFlow.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+const taskDesc = document.querySelector("#taskDesc");
+const taskTitle = document.querySelector("#taskTitle");
+const addTask = document.querySelector("#addTask");
+const taskArr = [];
+addTask.addEventListener('click', ()=> {
+    const tdVal = taskDesc.value;
+    const ttVal = taskTitle.value;
+    if (ttVal === "" || tdVal === "") {
+        alert("Sprawdź czy któreś z pól nie jest puste!")
+    } else {
+        taskDesc.value = "";
+        taskTitle.value = "";
+    }
+    const task = {
+        title: ttVal,
+        description: tdVal,
+        status: "pending"
+    }
+    taskArr.push(task);
+    console.log(taskArr);
+})
